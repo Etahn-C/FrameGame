eb2370aaa92a57c34662d3fec6b7ec7e73564401
Etahn-C <coutoethan@outlook.com>
2025-11-23 17:50:23 -0500

    Added Dialogue and File inputs

---
+++ main.py
@@ -0,0 +1,54 @@
+from tkinter import filedialog
+
+
+def get_response(question, options, outputs):
+    while True:
+        user_input = input(question)
+        if user_input.lower() in options:
+            return outputs[options.index(user_input)]
+        else:
+            print("Not a valid input, try again")
+        
+
+def get_settings(vid_dir="", frame_dir="", frame_timing=None):
+    while True:
+        
+        if vid_dir == "":
+            print("A file choice dialogue will appear, choose location where video files are")
+            input("press enter to continue")
+            while vid_dir == "":
+                vid_dir = filedialog.askdirectory(title="Choose location where videos are", mustexist=True)
+        elif get_response("change video directory? (Y/n): ", ['y','n'], [True, False]):
+            vid_dir = ""
+            while vid_dir == "":
+                vid_dir = filedialog.askdirectory(title="Choose location where videos are", mustexist=True)
+            
+        if frame_dir == "":
+            print("A file choice dialogue will appear, choose location where you want frames and data to be saved")
+            input("press enter to continue")
+            while frame_dir == "": 
+                frame_dir = filedialog.askdirectory(title="Choose frame/data saving location", mustexist=True)
+        elif get_response("change frame/data directory? (Y/n): ", ['y','n'], [True, False]):
+            frame_dir = ""
+            while frame_dir == "": 
+                frame_dir = filedialog.askdirectory(title="Choose frame/data saving location", mustexist=True)
+            
+        if frame_timing is None:
+            frame_timing = get_response("Do you want random frame timings? (Y/n): ", ['y','n'], [True, False])
+        elif get_response("change frame timing setting? (Y/n): ", ['y','n'], [True, False]):
+            frame_timing = get_response("Do you want random frame timings? (Y/n): ", ['y','n'], [True, False])
+        
+        if not get_response(f"Here are your current settings:\n\tVideo Directory ------- {vid_dir}\n\tFrame/Data Directory -- {frame_dir}\n\tRandom Frame Times ---- {frame_timing}\nwould you like to change them? (Y/n): ", ['y','n'], [True, False]):
+            return vid_dir, frame_dir, frame_timing
+    
+
+def main():
+    print("--Welcome to FrameExtractor for FrameGame!--")
+    input("press enter to continue")
+    vid_dir, frame_dir, frame_timing = get_settings()
+    print (f"Here are your current settings:\n\tVideo Directory ------- {vid_dir}\n\tFrame/Data Directory -- {frame_dir}\n\tRandom Frame Times ---- {frame_timing}")
+    input("hit enter to exit")
+
+
+if __name__ == "__main__":
+    main()
