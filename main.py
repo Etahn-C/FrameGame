a1df4002c03c527f7afcd42d87d58a071d57fb72
Etahn-C <coutoethan@outlook.com>
2025-11-24 13:48:58 -0500

    Added FFMPEG

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import filedialog
 from random import randrange
+import ffmpeg
 
 
 def get_response(question, options, outputs):
@@ -59,16 +60,26 @@ def frame_times(frame_timing, length, fps=1/60):
 
     return frames
 
+def extract_frames(file=0, output_loc=0, frames=0):
+    (
+        ffmpeg
+        .input(r"C:\Users\Couto\Personal\Coding\FrameGame\test-folder\video\My Little Pony Friendship Is Magic S01E01.mkv")
+        .output(r"C:\Users\Couto\Personal\Coding\FrameGame\test-folder\frame-data\out%d.jpg", vf="select='between(t,60,61)+between(t,120,121)+between(t,180,181)+between(t,240,241)+between(t,300,301)+between(t,360,361)+between(t,420,421)+between(t,480,481)+between(t,540,541)+between(t,600,601)+between(t,660,661)+between(t,720,721)+between(t,780,781)+between(t,840,841)+between(t,900,901)+between(t,960,961)+between(t,1020,1021)+between(t,1080,1081)+between(t,1140,1141)+between(t,1200,1201)+between(t,1260,1261)+between(t,1320,1321)+between(t,1380,1381)',fps=1/61")
+        .run()
+    )
+    
+    
+
 
 def main():
     print("--Welcome to FrameExtractor for FrameGame!--")
     input("press enter to continue")
-    vid_dir, frame_dir, frame_timing = get_settings()
-    print (f"Here are your current settings:\n\tVideo Directory ------- {vid_dir}\n\tFrame/Data Directory -- {frame_dir}\n\tRandom Frame Times ---- {frame_timing}")
+    #vid_dir, frame_dir, frame_timing = get_settings()
+    #print (f"Here are your current settings:\n\tVideo Directory ------- {vid_dir}\n\tFrame/Data Directory -- {frame_dir}\n\tRandom Frame Times ---- {frame_timing}")
 
-    print(frame_times(False, 300, 1/60))
+    #print(frame_times(False, 300, 1/60))
     print(frame_times(True, 300, 1/60))
-
+    extract_frames()
     input("hit enter to exit")
 
 
