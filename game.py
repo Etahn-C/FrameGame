cd1226072936846bdcf990f162414727dc1aea0a
Etahn-C <coutoethan@outlook.com>
2025-12-14 10:47:28 -0500

    Settings Widgets Setup

--- game.py
+++ game.py
@@ -108,31 +108,31 @@ class main_screen(tk.Frame):
         # Title bar
         self.title_bar = tk.Label(
             self, text="Frame Game", bg=self.shared_data["bg-color"])
-        self.title_bar.place(relheight=self.shared_data["y-mod"]*1,
-                             relwidth=self.shared_data["x-mod"]*28,
-                             relx=self.shared_data["x-mod"]*3,
-                             rely=self.shared_data["y-mod"]*0)
+        self.title_bar.place(relheight=1*self.shared_data["y-mod"],
+                             relwidth=28*self.shared_data["x-mod"],
+                             relx=3*self.shared_data["x-mod"],
+                             rely=0*self.shared_data["y-mod"])
 
         # Image Info
         self.image_info_label = tk.Label(
             self, bg=self.shared_data["bg-color"])
-        self.image_info_label.place(relheight=self.shared_data["y-mod"],
+        self.image_info_label.place(relheight=1*self.shared_data["y-mod"],
                                     relwidth=24*self.shared_data["x-mod"],
                                     relx=1*self.shared_data["x-mod"],
                                     rely=19*self.shared_data["y-mod"])
 
         # Score Info
         self.score_label = tk.Label(self, bg=self.shared_data["bg-color"])
-        self.score_label.place(relheight=self.shared_data["y-mod"],
+        self.score_label.place(relheight=1*self.shared_data["y-mod"],
                                relwidth=7*self.shared_data["x-mod"],
                                relx=26*self.shared_data["x-mod"],
                                rely=19*self.shared_data["y-mod"])
 
         # Search Bar
         self.search_bar = tk.Entry(self, bg=self.shared_data["bt-color"])
-        self.search_bar.place(relheight=self.shared_data["y-mod"],
+        self.search_bar.place(relheight=1*self.shared_data["y-mod"],
                               relwidth=24*self.shared_data["x-mod"],
-                              relx=self.shared_data["x-mod"],
+                              relx=1*self.shared_data["x-mod"],
                               rely=20*self.shared_data["y-mod"])
         # Search Menu Canvas
         self.search_menu_canvas = tk.Canvas(
@@ -226,8 +226,8 @@ class main_screen(tk.Frame):
         self.image_canvas = tk.Canvas(self, bg=self.shared_data["bg-color"])
         self.image_canvas.place(relheight=18*self.shared_data["y-mod"],
                                 relwidth=32*self.shared_data["x-mod"],
-                                relx=self.shared_data["x-mod"],
-                                rely=self.shared_data["y-mod"])
+                                relx=1*self.shared_data["x-mod"],
+                                rely=1*self.shared_data["y-mod"])
 
     def next_skip(self):
         """Will start game, prepare for new frame, or skip frame"""
@@ -239,7 +239,7 @@ class main_screen(tk.Frame):
 
     def settings_page(self):
         """Switches the page to the settings page"""
-        self.controller.shared_data = self.shared_data
+        # self.controller.shared_data = self.shared_data
         self.controller.show_page(settings_screen)
         pass
 
@@ -294,16 +294,146 @@ class settings_screen(tk.Frame):
         super().__init__(parent, bg=controller.shared_data["bg-color"])
         self.shared_data = controller.shared_data
         self.controller = controller
-        self.draw_widgets()
         self.update_settings()
+        self.draw_widgets()
 
     def on_show(self):
         """Runs when the page is shown"""
-        pass
+
+        self.update_widgets()
 
     def draw_widgets(self):
         """Draws all the widgets used for the main screen"""
-        pass
+
+        # Title bar
+        title_bar = tk.Label(self, text="Frame Game: Settings",
+                             bg=self.shared_data["bg-color"])
+        title_bar.place(relheight=1*self.shared_data["y-mod"],
+                        relwidth=28*self.shared_data["x-mod"],
+                        relx=3*self.shared_data["x-mod"],
+                        rely=0*self.shared_data["y-mod"])
+
+        # Settings Labels
+        file_label = tk.Label(self, text="Change File",
+                              bg=self.shared_data["bg-color"],
+                              relief="sunken")
+        file_label.place(relheight=1*self.shared_data["y-mod"],
+                         relwidth=8*self.shared_data["x-mod"],
+                         relx=1*self.shared_data["x-mod"],
+                         rely=2*self.shared_data["y-mod"])
+
+        season_label = tk.Label(self, text="Season Select",
+                                bg=self.shared_data["bg-color"],
+                                relief="sunken")
+        season_label.place(relheight=1*self.shared_data["y-mod"],
+                           relwidth=8*self.shared_data["x-mod"],
+                           relx=1*self.shared_data["x-mod"],
+                           rely=5*self.shared_data["y-mod"])
+
+        synopsis_label = tk.Label(self, text="Synopsis Search",
+                                  bg=self.shared_data["bg-color"],
+                                  relief="sunken")
+        synopsis_label.place(relheight=1*self.shared_data["y-mod"],
+                             relwidth=8*self.shared_data["x-mod"],
+                             relx=1*self.shared_data["x-mod"],
+                             rely=8*self.shared_data["y-mod"])
+
+        report_label = tk.Label(self, text="Report Threshold",
+                                bg=self.shared_data["bg-color"],
+                                relief="sunken")
+        report_label.place(relheight=1*self.shared_data["y-mod"],
+                           relwidth=8*self.shared_data["x-mod"],
+                           relx=1*self.shared_data["x-mod"],
+                           rely=11*self.shared_data["y-mod"])
+
+        # File Button
+        self.file_button = tk.Button(self, anchor='w',
+                                     text=self.shared_data["game-dir"],
+                                     bg=self.shared_data["bt-color"],
+                                     command=self.change_file)
+        self.file_button.place(relheight=1*self.shared_data["y-mod"],
+                               relwidth=23*self.shared_data["x-mod"],
+                               relx=10*self.shared_data["x-mod"],
+                               rely=2*self.shared_data["y-mod"])
+
+        self.file_button_label = tk.Label(self,
+                                          text="Click ^ To Choose Directory",
+                                          bg=self.shared_data["bg-color"],
+                                          anchor='w')
+        self.file_button_label.place(relheight=1*self.shared_data["y-mod"],
+                                     relwidth=23*self.shared_data["x-mod"],
+                                     relx=10*self.shared_data["x-mod"],
+                                     rely=3*self.shared_data["y-mod"])
+
+        # Season Entry
+        self.season_entry = tk.Entry(self, relief='raised',
+                                     bg=self.shared_data["bt-color"])
+        self.season_entry.place(relheight=1*self.shared_data["y-mod"],
+                                relwidth=23*self.shared_data["x-mod"],
+                                relx=10*self.shared_data["x-mod"],
+                                rely=5*self.shared_data["y-mod"])
+
+        self.season_entry_label = tk.Label(self, anchor='w',
+                                           bg=self.shared_data["bg-color"],
+                                           text="Must be in '1, 2, 5' format")
+        self.season_entry_label.place(relheight=1*self.shared_data["y-mod"],
+                                      relwidth=23*self.shared_data["x-mod"],
+                                      relx=10*self.shared_data["x-mod"],
+                                      rely=6*self.shared_data["y-mod"])
+
+        # Synopsis Radios
+        self.syn = tk.BooleanVar(self, value=True)
+        self.synopsis_radio_yes = tk.Radiobutton(
+            self, text="Yes", bg=self.shared_data["bt-color"],
+            variable=self.syn, value=True, indicatoron=False)
+        self.synopsis_radio_yes.place(relheight=1*self.shared_data["y-mod"],
+                                      relwidth=3*self.shared_data["x-mod"],
+                                      relx=10*self.shared_data["x-mod"],
+                                      rely=8*self.shared_data["y-mod"])
+
+        self.synopsis_radio_no = tk.Radiobutton(
+            self, text="No", bg=self.shared_data["bt-color"],
+            variable=self.syn, value=False, indicatoron=False)
+        self.synopsis_radio_no.place(relheight=1*self.shared_data["y-mod"],
+                                     relwidth=3*self.shared_data["x-mod"],
+                                     relx=13*self.shared_data["x-mod"],
+                                     rely=8*self.shared_data["y-mod"])
+
+        # Report Entry
+        self.report_entry = tk.Entry(self, relief='raised',
+                                     bg=self.shared_data["bt-color"])
+        self.report_entry.place(relheight=1*self.shared_data["y-mod"],
+                                relwidth=4*self.shared_data["x-mod"],
+                                relx=10*self.shared_data["x-mod"],
+                                rely=11*self.shared_data["y-mod"])
+
+        # Save Button
+        self.save_button = tk.Button(self, text="Save Settings",
+                                     bg=self.shared_data["bt-color"],
+                                     command=self.save_settings)
+        self.save_button.place(relheight=1*self.shared_data["y-mod"],
+                               relwidth=14*self.shared_data["x-mod"],
+                               relx=10*self.shared_data["x-mod"],
+                               rely=14*self.shared_data["y-mod"])
+
+        # Return Button
+        self.return_button = tk.Button(self, text="Return to Main Menu",
+                                       bg=self.shared_data["bt-color"],
+                                       command=self.to_main_screen)
+        self.return_button.place(relheight=1*self.shared_data["y-mod"],
+                                 relwidth=14*self.shared_data["x-mod"],
+                                 relx=10*self.shared_data["x-mod"],
+                                 rely=17*self.shared_data["y-mod"])
+
+        # Default Button
+        self.default_button = tk.Button(self, text="Default Settings",
+                                        fg="#AA0000",
+                                        bg=self.shared_data["bt-color"],
+                                        command=self.default_settings)
+        self.default_button.place(relheight=1*self.shared_data["y-mod"],
+                                  relwidth=10*self.shared_data["x-mod"],
+                                  relx=12*self.shared_data["x-mod"],
+                                  rely=27*self.shared_data["y-mod"])
 
     def update_settings(self):
         """Updates the settings"""
@@ -347,7 +477,7 @@ class settings_screen(tk.Frame):
             regex = r"([Ss]0?[1-9]\d*)([Ee]0?[1-9]\d*)"
             for key in list(self.shared_data["all-ep-data"].keys()):
                 if int(re.search(regex, key).group(1)[1:]) not in (
-                           self.shared_data["allowed-seasons"]):
+                        self.shared_data["allowed-seasons"]):
                     del self.shared_data["all-ep-data"][key]
 
             # Title map
@@ -361,6 +491,41 @@ class settings_screen(tk.Frame):
                                                 for ep_id, info
                                                 in self.shared_data
                                                 ["all-ep-data"].items()}
+
+    def update_widgets(self):
+        """
+        Enables or Disables Widgets
+        """
+        if self.shared_data["game-dir"] == "":
+            self.season_entry.config(state=tk.DISABLED)
+            self.synopsis_radio_yes.config(state=tk.DISABLED)
+            self.synopsis_radio_no.config(state=tk.DISABLED)
+            self.report_entry.config(state=tk.DISABLED)
+            self.save_button.config(state=tk.DISABLED)
+            self.default_button.config(state=tk.DISABLED)
+        else:
+            self.season_entry.config(state=tk.NORMAL)
+            self.synopsis_radio_yes.config(state=tk.NORMAL)
+            self.synopsis_radio_no.config(state=tk.NORMAL)
+            self.report_entry.config(state=tk.NORMAL)
+            self.save_button.config(state=tk.NORMAL)
+            self.default_button.config(state=tk.NORMAL)
+
+    def default_settings(self):
+        """Generates default settings from data file"""
+        pass
+
+    def to_main_screen(self):
+        """Switches screen to main screen"""
+        pass
+
+    def save_settings(self):
+        """Saves new settings to file"""
+        pass
+
+    def change_file(self):
+        """Updates the current game file"""
+        pass
 
 
 class leaderboard_screen(tk.Frame):
@@ -376,6 +541,7 @@ class leaderboard_screen(tk.Frame):
 
     def on_show(self):
         """Runs when the page is shown"""
+        print(self.shared_data["game-dir"])
         pass
 
     def draw_widgets(self):
